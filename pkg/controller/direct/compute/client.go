5235068e93116316f0540efd44eec9f9d796e99f
Fiza Shaikh <fiza.shaikh@workday.com>
2025-07-22 13:28:03 +1200

    feature-future-reservations: Add controller

--- pkg/controller/direct/compute/client.go
+++ pkg/controller/direct/compute/client.go
@@ -22,9 +22,8 @@ import (
 	"context"
 	"fmt"
 
-	"google.golang.org/api/option"
-
 	compute "cloud.google.com/go/compute/apiv1"
+	computev1beta "cloud.google.com/go/compute/apiv1beta"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
 )
 
@@ -105,7 +104,6 @@ func (m *gcpClient) newTargetTcpProxiesClient(ctx context.Context) (*compute.Tar
 }
 
 func (m *gcpClient) newRegionalTargetTcpProxiesClient(ctx context.Context) (*compute.RegionTargetTcpProxiesClient, error) {
-	var opts []option.ClientOption
 	opts, err := m.config.RESTClientOptions()
 	if err != nil {
 		return nil, err
@@ -113,7 +111,18 @@ func (m *gcpClient) newRegionalTargetTcpProxiesClient(ctx context.Context) (*com
 	client, err := compute.NewRegionTargetTcpProxiesRESTClient(ctx, opts...)
 	if err != nil {
 		return nil, fmt.Errorf("building compute RegionalTargetTcpProxiesClient client: %w", err)
+	}
+	return client, err
+}
 
+func (m *gcpClient) newFutureReservationsClient(ctx context.Context) (*computev1beta.FutureReservationsClient, error) {
+	opts, err := m.config.RESTClientOptions()
+	if err != nil {
+		return nil, err
+	}
+	client, err := computev1beta.NewFutureReservationsRESTClient(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("building compute futureReservations client: %w", err)
 	}
 	return client, err
 }
